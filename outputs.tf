d4d2ceee4cd91605b3ff515adb4e21fe404c1923
Alexander Mancevice <amancevice@cargometrics.com>
2018-10-27 20:42:54 -0400

    rename sns outputs

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output sns_topic_arn {
-  description = "SNS topic ARN."
-  value       = "${aws_sns_topic.topic.name}"
-}
-
-output sns_topic_name {
-  description = "SNS topic name."
-  value       = "${aws_sns_topic.topic.name}"
-}
-
 output lambda_arn {
   description = "Lambda function ARN."
   value       = "${aws_lambda_function.lambda.arn}"
@@ -16,4 +6,14 @@ output lambda_arn {
 output lambda_name {
   description = "Lambda function name."
   value       = "${aws_lambda_function.lambda.function_name}"
+}
+
+output topic_arn {
+  description = "SNS topic ARN."
+  value       = "${aws_sns_topic.topic.name}"
+}
+
+output topic_name {
+  description = "SNS topic name."
+  value       = "${aws_sns_topic.topic.name}"
 }
