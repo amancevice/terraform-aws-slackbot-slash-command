92363f7f2a2b31efb709ad1cfc580bfec91e9634
Alexander Mancevice <amancevice@cargometrics.com>
2020-02-10 08:45:23 -0500

    add more outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output lambda {
   description = "Slash command Lambda function."
   value       = aws_lambda_function.lambda
 }
+
+output logs {
+  description = "Lambda function CloudWatch log group."
+  value       = aws_cloudwatch_log_group.logs
+}
+
+output permission {
+  description = "Lambda invocation permission."
+  value       = aws_lambda_permission.invoke
+}
+
+output subscription {
+  description = "SNS subscription."
+  value       = aws_sns_topic_subscription.subscription
+}
