66dc3b9dfa4dc821edc15ea35d68ea5ced207f2f
Alexander Mancevice <alexander.mancevice@hey.com>
2020-07-27 11:16:34 -0400

    bugfix

--- index.js
+++ index.js
@@ -21,16 +21,19 @@ const handle = async (record) => {
 
   // Dialog response (deprecated)
   if (response.response_type === 'dialog') {
+    console.log(`${response.type.toUpperCase()} ${payload.trigger_id}`);
     return slack.dialog.open({dialog: response, trigger_id: payload.trigger_id});
   }
 
   // Modal response
   else if (response.type === 'modal') {
+    console.log(`${response.type.toUpperCase()} ${payload.trigger_id}`);
     return slack.views.open({view: response, trigger_id: payload.trigger_id});
   }
 
   // Direct response
   else {
+    console.log(`POST ${payload.response_url}`);
     response.channel = payload.channel_id;
     return axios.post(payload.response_url, response);
   }
@@ -39,5 +42,5 @@ const handle = async (record) => {
 exports.handler = async (event) => {
   console.log(`EVENT ${JSON.stringify(event)}`);
   await Promise.resolve(slack || getSlack({SecretId: AWS_SECRET}));
-  return await Promise.all(event.Records.map(handle));
+  await Promise.all(event.Records.map(handle));
 };
